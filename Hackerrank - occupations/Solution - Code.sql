6e04b46bf05dcb4c425e8930f34a3b215d25ab0b
Caio di Felice Cunha <111542025+Caio-Felice-Cunha@users.noreply.github.com>
2023-04-20 11:12:05 -0300

    Update Solution - Code.sql

--- Hackerrank - occupations/Solution - Code.sql
+++ Hackerrank - occupations/Solution - Code.sql
@@ -1,23 +1,23 @@
 ## Problem: https://github.com/Caio-Felice-Cunha/SQl_Problems/blob/main/Hackerrank%20-%20occupations/Problem.md
 ## My Solution
 
-select
-    min(
-        if(occupation = 'Doctor',name,null)) as Doctor,
-    min(
-        if(occupation = 'Professor',name,null)) as Professor,
-    min(
-        if(occupation = 'Singer',name,null)) as Singer,
-    min(
-        if(occupation = 'Actor',name,null)) as Actor
-from
-    (select 
+SELECT
+    MIN(
+        if(occupation = 'Doctor', name, null)) AS Doctor,
+    MIN(
+        if(occupation = 'Professor', name, null)) AS Professor,
+    MIN(
+        if(occupation = 'Singer', name, null)) AS Singer,
+    MIN(
+        if(occupation = 'Actor', name, null)) AS Actor
+FROM
+    (SELECT 
         name,
         occupation,
-        Row_number() 
-            Over (
-                partition by occupation 
-                order by name) as row_num 
-     from 
-        occupations) as ord 
-group by row_num;
+        ROW_NUMBER() 
+            OVER (
+                PARTITION BY occupation 
+                ORDER BY name) as NumberRow 
+     FROM 
+        occupations) AS Order 
+GROUP BY NumberRow;
