ce77a7fec2d5f39b1fed830fc36176d52121c81d
Caio di Felice Cunha <111542025+Caio-Felice-Cunha@users.noreply.github.com>
2023-04-20 10:55:44 -0300

    Create Solution - Code.sql

---
+++ StrataScratch - Popularity Percentage/Solution - Code.sql
@@ -0,0 +1,33 @@
+-- My Solution:
+
+WITH PairsUniqueCTE AS (
+    SELECT 
+        DISTINCT user1 AS UserId 
+    FROM 
+        facebook_friends
+  UNION
+    SELECT 
+        DISTINCT user2 AS UserId 
+    FROM 
+        facebook_friends
+),
+CountFriendsCTE AS (
+    SELECT 
+        UserId,
+        COUNT(*) AS TotalFriends
+    FROM (
+        SELECT user1 AS UserId FROM facebook_friends
+    UNION ALL
+        SELECT user2 AS UserId FROM facebook_friends) AS TotalUsers
+    GROUP BY 
+        UserId
+)
+SELECT 
+  PairsUniqueCTE.UserId,
+  (CountFriendsCTE.TotalFriends 
+    / (SELECT COUNT(DISTINCT UserId) FROM PairsUniqueCTE)) * 100 AS popularity_percent
+FROM 
+    PairsUniqueCTE
+        LEFT JOIN 
+    CountFriendsCTE USING(UserId)
+ORDER BY  PairsUniqueCTE.UserId ASC;
